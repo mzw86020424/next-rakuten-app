af0968c79f62e2e7ec58ad893817ca984f601c35
mzw86020424 <mzw86020424@gmail.com>
2023-04-15 15:18:22 +0900

    ざっくり検索フォーム実装

---
+++ app/pages/hotels.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function Hotels() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    console.log(searchTerm);
+  };
+
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={(x) => setSearchTerm(x.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
