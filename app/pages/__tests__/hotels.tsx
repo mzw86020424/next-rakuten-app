c41df956815941911086dc2cba8676d795b5de6c
mzw86020424 <mzw86020424@gmail.com>
2023-04-17 09:25:25 +0900

    テストを修正

--- app/pages/__tests__/hotels.tsx
+++ app/pages/__tests__/hotels.tsx
@@ -1,25 +1,23 @@
 import Hotels from '../hotels'
 import { render, fireEvent } from '@testing-library/react'
 
-describe('hotelsコンポーネント', () => {
-  describe('初期表示', () => {
-    test('入力フォームと検索ボタンが表示されている', () => {
-      const { getByText, getByPlaceholderText } = render(<Hotels />)
-      expect(getByPlaceholderText('text')).toBeTruthy()
-      expect(getByText('Search')).toBeTruthy()
-    })
+describe('初期表示', () => {
+  test('入力フォームと検索ボタンが表示されている', () => {
+    const { getByText, getByPlaceholderText } = render(<Hotels />)
+    expect(getByPlaceholderText('text')).toBeTruthy()
+    expect(getByText('Search')).toBeTruthy()
   })
-  describe('検索ボタン押下', () => {
-    test('フォームに入力して検索ボタンを押下するとconsole.logされる', () => {
-      const { getByText, getByPlaceholderText } = render(<Hotels />)
-      const input = getByPlaceholderText('text')
-      const button = getByText('Search')
-      const searchString = 'test'
-      input.value = searchString
-      expect(input.value).toBe(searchString)
-      fireEvent.click(button)
-      const consoleOutput = []
-      console.log = jest.fn((output) => consoleOutput.push(output))
-    })
+})
+describe('検索ボタン押下', () => {
+  test('フォームに入力して検索ボタンを押下するとconsole.logされる', () => {
+    const { getByText, getByPlaceholderText } = render(<Hotels />)
+    const input = getByPlaceholderText('text')
+    const button = getByText('Search')
+    const searchString = 'test'
+    input.value = searchString
+    expect(input.value).toBe(searchString)
+    fireEvent.click(button)
+    const consoleOutput = []
+    console.log = jest.fn((output) => consoleOutput.push(output))
   })
 })
