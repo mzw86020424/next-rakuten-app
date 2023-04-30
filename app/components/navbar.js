11aac84234720f55f955c47352b0a3247afb9194
mzw86020424 <mzw86020424@gmail.com>
2023-05-01 08:47:22 +0900

    ヘッダーを仮置き

---
+++ app/components/navbar.js
@@ -0,0 +1,21 @@
+import styles from './navbar.module.css'
+import NextLink from 'next/link'
+
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_title}>
+        <NextLink href="/">サイトのタイトル</NextLink>
+      </div>
+      <ul>
+        <li>
+          <NextLink href="/" className={styles.navbar_link}>ホーム</NextLink>
+        </li>
+        <li>
+          <NextLink href="/hotels" className={styles.navbar_link}>ホテル</NextLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
