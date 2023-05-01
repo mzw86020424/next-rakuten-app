f9db471c2641c6af003254529471b7583dc44f5e
mzw86020424 <mzw86020424@gmail.com>
2023-05-01 09:06:58 +0900

    表示内容のテストを実装

---
+++ app/__tests__/components/navbar.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/navbar'
+import { render } from '@testing-library/react'
+
+describe('Navbarコンポーネント', () => {
+  test('サイトのタイトルが表示される', () => {
+    const { getByText } = render(<Navbar />)
+    expect(getByText('サイトのタイトル')).toBeTruthy()
+  })
+
+  test('ホームとホテルへのリンクが表示される', () => {
+    const { getByText } = render(<Navbar />)
+    expect(getByText('ホーム')).toBeTruthy()
+    expect(getByText('ホテル')).toBeTruthy()
+  })
+})
