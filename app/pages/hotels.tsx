e37e365511336430fd90a37cc8d8a3cd29b16132
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-05-07 11:09:35 +0900

    ✨ ホテルサーチAPI作成(test未実装)

--- app/pages/hotels.tsx
+++ app/pages/hotels.tsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
+import { Hotel } from 'type/hotel.type'
+import axios from 'axios'
 
-export default function Hotels() {
-  const [searchTerm, setSearchTerm] = useState('')
+type SearchResponse = {
+  hotels: Hotel[]
+}
 
-  const handleSearch = () => {
-    console.log(searchTerm)
+const HotelSearch = () => {
+  const [query, setQuery] = useState('')
+  const [hotels, setHotels] = useState<Hotel[]>([])
+
+  const handleSearch = async (): Promise<SearchResponse> => {
+    const { data } = await axios.get<SearchResponse>(
+      `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&keyword=${query}&applicationId=${process.env.NEXT_PUBLIC_RAKUTEN_ID}`
+    )
+    return data
+  }
+
+  const handleButtonClick = async () => {
+    try {
+      const data = await handleSearch()
+      setHotels(data.hotels)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
@@ -12,10 +31,19 @@ export default function Hotels() {
       <input
         placeholder="text"
         type="text"
-        value={searchTerm}
-        onChange={(x) => setSearchTerm(x.target.value)}
+        value={query}
+        onChange={(x) => setQuery(x.target.value)}
       />
-      <button onClick={handleSearch}>Search</button>
+      {/* eslint-disable-next-line */}
+      <button onClick={handleButtonClick}>Search</button>
+      {hotels.map((value: Hotel) => (
+        <div key={value.hotel[0].hotelBasicInfo.hotelNo}>
+          <h2>{value.hotel[0].hotelBasicInfo.hotelName}</h2>
+          <p>{value.hotel[0].hotelBasicInfo.hotelInformationUrl}</p>
+        </div>
+      ))}
     </div>
   )
 }
+
+export default HotelSearch
