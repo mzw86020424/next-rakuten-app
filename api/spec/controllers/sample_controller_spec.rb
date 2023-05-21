2ee44dc7a1305a25c695ef26ee22e6f36c46c19b
mzw86020424 <mzw86020424@gmail.com>
2023-05-21 16:30:54 +0900

    :recycle: apply rubocop / to clean up cords / #40

--- api/spec/controllers/sample_controller_spec.rb
+++ api/spec/controllers/sample_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe SamplesController do
   describe 'GET #index' do
     context 'sampleのデータが存在する場合' do
-      let!(:sample_1) { Sample.create! }
-      let!(:sample_2) { Sample.create! }
-      let!(:sample_3) { Sample.create! }
+      let!(:sample1) { Sample.create! }
+      let!(:sample2) { Sample.create! }
+      let!(:sample3) { Sample.create! }
 
       it 'Sampleテーブルの全てのレコードが取得できる' do
         get :index
         expect(response).to have_http_status(:success)
-        expect(response.body).to include(sample_1.id.to_s, sample_2.id.to_s, sample_3.id.to_s)
+        expect(response.body).to include(sample1.id.to_s, sample2.id.to_s, sample3.id.to_s)
       end
     end
   end
