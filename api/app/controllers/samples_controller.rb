c01d364415055dd4bfb9c6155baba43311869dd4
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-06-10 16:55:01 +0900

    Revert ":bug: Reverted test file for rspec."
    This reverts commit b6e3cf2803934e0b785a9fb63e52338a0bf1a77e.

--- api/app/controllers/samples_controller.rb
+++
@@ -1,7 +0,0 @@
-class SamplesController < SecuredController
-  skip_before_action :authorize_request
-
-  def index
-    render json: Sample.all
-  end
-end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :samples, only: [:index]
   namespace :api do
     namespace :v1 do
       resources :posts
