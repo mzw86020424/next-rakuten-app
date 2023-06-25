b022c0a06f9d7ffd60c11897d64d32aeb9bf5119
mzw86020424 <mzw86020424@gmail.com>
2023-06-25 12:51:39 +0900

    :seedling: add navabar test / to confirm correct transition / #32

--- frontend/__tests__/components/navbar.tsx
+++ frontend/__tests__/components/navbar.tsx
@@ -12,4 +12,20 @@ describe('Navbarコンポーネント', () => {
     expect(getByText('ホーム')).toBeTruthy()
     expect(getByText('ホテル')).toBeTruthy()
   })
+
+  test('ホームをクリックすると正しいURLに遷移する', () => {
+    const { getByText } = render(<Navbar />);
+    // "'homeLink' は 'null' の可能性があります。"の警告を回避するために、nullの場合は空のaタグを返す
+    const homeLink = getByText('ホーム').closest('a') ?? document.createElement('a');
+
+    expect(homeLink.getAttribute('href')).toBe('/');
+  });
+
+  test('ホテルをクリックすると正しいURLに遷移する', () => {
+    const { getByText } = render(<Navbar />);
+    // "'hotelLink' は 'null' の可能性があります。"の警告を回避するために、nullの場合は空のaタグを返す
+    const hotelLink = getByText('ホテル').closest('a') ?? document.createElement('a');
+
+    expect(hotelLink.getAttribute('href')).toBe('/hotels');
+  });
 })
