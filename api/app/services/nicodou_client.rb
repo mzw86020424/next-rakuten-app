1bff98b561537f3105abc397d877142d1f4a9faa
mzw86020424 <mzw86020424@gmail.com>
2023-07-09 16:03:14 +0900

    :recycle: ニコ動用クライアントのリファクタ / 取得の共通処理を切り出し / #65

--- api/app/services/nicodou_client.rb
+++ api/app/services/nicodou_client.rb
@@ -2,21 +2,33 @@
 class NicodouClient
   require 'net/http'
 
-  def search_videos
-    request = Net::HTTP::Get.new(
-      '/api/v2/snapshot/video/contents/search'
-    )
-    response = client.request(request)
-    JSON.parse(response.body)
+  def search_videos(keyword)
+    endpoint = 'snapshot/video/contents/search'
+
+    params = {
+      'q' => keyword,
+      # ソート順、取得項目、サービス名はとりあえず固定
+      '_sort' => 'viewCounter',
+      'targets' => 'title,description,tags',
+      '_context' => 'my-api'
+    }
+
+    res = get(endpoint, params)
+
+    JSON.parse(res.body)
   end
 
   private
 
-  def client
-    uri = URI.parse('https://api.search.nicovideo.jp')
-    client = Net::HTTP.new(uri.host, uri.port)
-    client.use_ssl = true
+  def get(endpoint, params={})
+    uri = URI.parse("https://api.search.nicovideo.jp/api/v2/#{endpoint}")
+    uri.query = URI.encode_www_form(params)
 
-    client
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Get.new(uri)
+
+    http.request(req)
   end
 end
