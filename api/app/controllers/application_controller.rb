09105f64006860dcee2af077d9f5c129fa72750a
mzw86020424 <mzw86020424@gmail.com>
2023-07-25 09:12:07 +0900

    :seedling: create error method / notify any 500 error / #68

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :render_and_notify_500
+
+  private
+
+  def render_and_notify_500(e)
+    render json: { error: e }, status: :internal_server_error
+    SlackClient.new('システムエラー通知').notify('500エラー', "#{e.message}\n#{e.backtrace.join("\n")}")
+  end
 end
