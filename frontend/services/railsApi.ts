a873c85de89888d610cda9e6b90fe17d0f4a9cd9
mzw86020424 <mzw86020424@gmail.com>
2023-08-01 08:20:39 +0900

    :fire: add method / for index videos / #74

--- frontend/services/railsApi.ts
+++ frontend/services/railsApi.ts
@@ -1,10 +1,15 @@
+const getNicodouVideos = async (params: RequestInit) => {
+  const path = 'nicodous'
+  return await fetchRailsApi(path, params)
+}
+
 const fetchRailsApi = async (path: string, options: RequestInit = {}) => {
   const url = createRailsApiUrl(path)
   const response = await fetch(url, options)
   if (!response.ok) {
     throw new Error('Network response was not ok')
   }
-  const data = (await response.json()) as unknown
+  const data = (await response.json())
   return data
 }
 
