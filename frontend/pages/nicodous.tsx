5c1994c9a321464b5ad31e585ba2c36375caefc4
mzw86020424 <mzw86020424@gmail.com>
2023-08-06 12:09:10 +0900

    :fire: add nicodou view / show nicodou response / #74

--- frontend/pages/nicodous.tsx
+++ frontend/pages/nicodous.tsx
@@ -1,19 +1,16 @@
+import { videoInfo } from '@/types/nicodous'
 import { useState } from 'react'
-import { getNicodouVideos } from "services/railsApi";
+import { getNicodouVideos } from 'services/railsApi'
 
 const Nicodous = () => {
-  const [keyword, setKeyword] = useState('')
-  const [nicodous, setNicodous] = useState<
-    {
-      // TODO:nicodouData型を定義する
-      nicodous: [nicodouData]
-    }[]
-  >([])
+  const [keyword, setKeyword] = useState<string>('')
+  const [nicodous, setNicodous] = useState<videoInfo[]>([])
 
   const fetchNicodous = async () => {
     try {
+      // TODO: resの型を指定する
       await getNicodouVideos(keyword).then((res) => {
-        setNicodous(res.nicodous)
+        setNicodous(res.data as videoInfo[])
       })
     } catch (error) {
       console.error(error)
@@ -33,7 +30,38 @@ const Nicodous = () => {
         onChange={(x) => setKeyword(x.target.value)}
       />
       <button onClick={handleSearchClick}>Search</button>
-      {{nicodous}}
+      <table>
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>詳細</th>
+            <th>タグ</th>
+          </tr>
+        </thead>
+        <tbody>
+          {nicodous.length === 0 ? (
+            <tr>
+              <td colSpan={3}>No results</td>
+            </tr>
+          ) : (
+            nicodous.map((value) => {
+              return (
+                <tr key={value.title}>
+                  <td>
+                    <h1>{value.title}</h1>
+                  </td>
+                  <td>
+                    <h2>{value.description}</h2>
+                  </td>
+                  <td>
+                    <h2>{value.tags}</h2>
+                  </td>
+                </tr>
+              )
+            })
+          )}
+        </tbody>
+      </table>
     </>
   )
 }
---
+++ frontend/types/nicodous.d.ts
@@ -0,0 +1,9 @@
+export type videoInfo = {
+  title: string
+  description: string
+  tags: string
+}
+
+export type videosInfoRes = {
+  data: videoInfo[]
+}
