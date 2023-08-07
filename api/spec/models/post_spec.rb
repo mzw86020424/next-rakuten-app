94f2b1d75b2b0f4af1c671a7e09dba35d4614307
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-08-08 08:39:30 +0900

    :sparkles: post_specをletを使ったものに修正/#71

--- api/spec/models/post_spec.rb
+++ api/spec/models/post_spec.rb
@@ -2,21 +2,50 @@ require 'rails_helper'
 
 RSpec.describe Post do
   describe 'validations' do
-    it 'titleが51文字以上のときはバリデーションエラーが返されること' do
-      post = Post.new(title: 'a' * 51)
-      expect(post).not_to be_valid
-      expect(post.errors[:title]).to include('is too long (maximum is 50 characters)')
+    let(:user) { create(:user)}
+    let(:post) { Post.new(user: user, title: title_content, caption: caption_content) }
+
+    context 'タイトルの長さによるテスト' do
+      let(:caption_content) { 'a' * 100 } # 複数のテストで再利用されるためデフォルトとして設定
+
+      context 'タイトルが51文字以上の場合' do
+        let(:title_content) { 'a' * 51 }
+
+        it 'バリデーションエラーが返されること' do
+          expect(post).not_to be_valid
+          expect(post.errors[:title]).to include('is too long (maximum is 50 characters)')
+        end
+      end
+
+      context 'タイトルが50文字の場合' do
+        let(:title_content) { 'a' * 50 }
+
+        it 'バリデーションエラーが表示されないこと' do
+          expect(post).to be_valid
+        end
+      end
     end
 
-    it 'captionが101文字以上のときはバリデーションエラーが返されること' do
-      post = Post.new(caption: 'a' * 101)
-      expect(post).not_to be_valid
-      expect(post.errors[:caption]).to include('is too long (maximum is 100 characters)')
-    end
+    context 'キャプションの長さによるテスト' do
+      let(:title_content) { 'a' * 50 } # 複数のテストで再利用されるためデフォルトとして設定
 
-    it 'titleが50文字、captionが100文字のときバリデーションエラーが表示されないこと' do
-      post = Post.new(user: create(:user), title: 'a' * 50, caption: 'a' * 100)
-      expect(post).to be_valid
+      context 'キャプションが101文字以上の場合' do
+        let(:caption_content) { 'a' * 101 }
+
+        it 'バリデーションエラーが返されること' do
+          expect(post).not_to be_valid
+          expect(post.errors[:caption]).to include('is too long (maximum is 100 characters)')
+        end
+      end
+
+      context 'キャプションが100文字の場合' do
+        let(:caption_content) { 'a' * 100 }
+
+        it 'バリデーションエラーが表示されないこと' do
+          expect(post).to be_valid
+        end
+      end
     end
   end
 end
+
